87ced7c338f47ebf85a27174d2619a701c258d51
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-12 08:36:51 +0545

    add register page UI with toastify

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { BrowseRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Chat from './pages/Chat'
 import Login from './pages/Login'
+import Register from './pages/Register'
 const App = () => {
   return (
- <BrowseRouter>
+ <BrowserRouter>
  <Routes>
   <Route path = "/login" element={<Login />} /> 
   <Route path = "/register" element={<Register />} />
   <Route path="/" element={<Chat />} />
  </Routes>
- </BrowseRouter>
+ </BrowserRouter>
 
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,129 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Register = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-export default Register
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleValidation();
+  };
+
+  const handleValidation = () => {
+    const { username, email, password, confirmPassword } = values;
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match!", {
+        position: "bottom-right",
+        autoClose: 7000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+      });
+    }
+  };
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className='brand'>
+            <img src={Logo} alt='Logo' />
+            <h1>ChatHub</h1>
+          </div>
+          <input type='text' placeholder='Username' name='username' onChange={(e) => handleChange(e)} />
+          <input type='email' placeholder='Email' name='email' onChange={(e) => handleChange(e)} />
+          <input type='password' placeholder='Password' name='password' onChange={(e) => handleChange(e)} />
+          <input type='password' placeholder='Confirm Password' name='confirmPassword' onChange={(e) => handleChange(e)} />
+          <button type='submit'>Create User</button>
+        </form>
+
+        <span>
+          Already have an account? <Link to='/login'>Login</Link>
+        </span>
+      </FormContainer>
+
+      <ToastContainer />
+    </>
+  );
+};
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+`;
+
+export default Register;
