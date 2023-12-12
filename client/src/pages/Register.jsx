ebac3a599c4e1367d0554aa2973dbd30a545a908
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-12 20:27:00 +0545

    completed register functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { registerRoute } from "../utils/APIRoutes";
 const Register = () => {
+  const navigate = useNavigate();
   const [values, setValues] = useState({
     username: "",
     email: "",
@@ -16,19 +17,28 @@ const Register = () => {
 
   const toastOptions = {
     position: "bottom-right",
-    autoClose: 7000,
+    autoClose: 6000,
     pauseOnHover: true,
     draggable: true,
     theme: "dark",
   };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     if (handleValidation()) {
-      console.log(registerRoute);
-      const { username, email, password } = values;
-      const { data } = await axios.post(registerRoute, { username, email, password });
+      const { email, username, password } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem(process.env.REACT_APP_LOCALHOST_KEY, JSON.stringify(data.user));
+        navigate("/");
+      }
     }
   };
   const handleValidation = () => {
--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -5,13 +5,9 @@ module.exports.register = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const usernameCheck = await User.findOne({ username });
-    if (usernameCheck) {
-      return res.status(400).json({ message: "Username already exists" });
-    }
+    if (usernameCheck) return res.json({ msg: "Username already used", status: false });
     const emailCheck = await User.findOne({ email });
-    if (emailCheck) {
-      return res.status(400).json({ message: "Email already exists" });
-    }
+    if (emailCheck) return res.json({ msg: "Email already used", status: false });
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
       email,
@@ -19,8 +15,8 @@ module.exports.register = async (req, res, next) => {
       password: hashedPassword,
     });
     delete user.password;
-    return res.status(201).json({ user });
-  } catch (error) {
-    next(error);
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
   }
 };
