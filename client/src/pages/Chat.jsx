86ad9353f0bbc7c9598b5735723d17bda0c36191
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-13 22:16:21 +0545

    chat and contacts added

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { allUsersRoute } from "../utils/APIRoutes";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
 const Chat = () => {
-  return (
-    <div>
-     CHAT 
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
 
-export default Chat
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!localStorage.getItem("chat-app-user")) {
+        navigate("/login");
+        return;
+      } else {
+        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+      }
+    };
+
+    fetchData();
+  }, [navigate]);
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (currentUser) {
+        try {
+          if (currentUser.isAvatarImageSet) {
+            const response = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+            setContacts(response.data);
+          } else {
+            navigate("/setavatar");
+          }
+        } catch (error) {
+          console.error("Error fetching contacts:", error);
+        }
+      }
+    };
+    fetchContacts();
+  }, [currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+  return (
+    <Container>
+      <div className='container'>
+        <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+        <Welcome currentUser={currentUser} />
+      </div>
+    </Container>
+  );
+};
+
+export default Chat;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
