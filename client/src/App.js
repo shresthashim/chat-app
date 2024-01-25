eead2b4155c14f5be8ec346cbaf8d55c82f33ae8
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-25 20:47:28 +0545

    feat : add pre loader spinner in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,53 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Chat from './pages/Chat'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import SetAvatar from './pages/SetAvatar'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SetAvatar from "./pages/SetAvatar";
+import { RingLoader } from "react-spinners";
+
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
- <BrowserRouter>
- <Routes>
-  <Route path = "/login" element={<Login />} /> 
-  <Route path = "/register" element={<Register />} />
-  <Route path="/" element={<Chat />} />
-  <Route path= "/setAvatar" element={<SetAvatar />} />
- </Routes>
- </BrowserRouter>
+    <>
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            backgroundColor: "#131324",
+          }}
+        >
+          <RingLoader
+            speedMultiplier='1'
+            color={"#ff9900"}
+            loading={loading}
+            size={100}
+            aria-label='Loading Spinner'
+            data-testid='loader'
+          />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Chat />} />
+            <Route path='/setAvatar' element={<SetAvatar />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
